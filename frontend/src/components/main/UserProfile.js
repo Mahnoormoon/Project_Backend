28dc7c1fac799fe9161d0d31e7dc4f034d87a5f4
Mahnoormoon <mahnoormoon530@gmail.com>
2023-03-18 16:50:01 +0530

    changes

--- frontend/src/components/main/UserProfile.js
+++ frontend/src/components/main/UserProfile.js
@@ -59,34 +59,6 @@ const UserProfile = () => {
                       <TimeClock />
                     </DemoItem>
                   </MDBCol>
-                  <MDBCol className="mb-2 w-100 rounded-3">
-                    {/*Weather Widget*/}
-                    ...
-                    <MDBCard
-                      className="bg-dark text-white"
-                      style={{ borderRadius: "40px" }}
-                    >
-                      <div className="bg-image" style={{ borderRadius: "35px" }}>
-                        <MDBCardImage
-                          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/draw1.webp"
-                          className="card-img"
-                          alt="weather"
-                        />
-                        <div
-                          className="mask"
-                          style={{ backgroundColor: "rgba(190, 216, 232, .5)" }}
-                        ></div>
-                      </div>
-                      <div className="card-img-overlay text-dark p-5">
-                        <MDBTypography tag="h4" className="mb-0">Juneau, Alaska, US</MDBTypography>
-                        <p className="display-2 my-3">1.28°C</p>
-                        <p className="mb-2">
-                          Feels Like: <strong>-1.08 °C</strong>
-                        </p>
-                        <MDBTypography tag="h5">Snowy</MDBTypography>
-                      </div>
-                    </MDBCard>
-                  </MDBCol>
                 </MDBRow>
                 <MDBCardBody className="text-black p-4 mt-10">
                   <MDBRow className="g-2 p-2">
