18f7151b3731cb93de8f60ce9fd8d031e746391d
Mahnoormoon <mahnoormoon530@gmail.com>
2023-03-28 11:27:20 +0530

    changes

--- frontend/src/components/user/ToDo.js
+++ frontend/src/components/user/ToDo.js
@@ -614,7 +614,7 @@ const ToDo = () => {
                                                             &nbsp;&nbsp;&nbsp;{new Date(task.created_at).toLocaleTimeString()}
                                                             &nbsp;&nbsp;&nbsp;<button className="btn btn-info">
                                                                 {" "}
-                                                                <i style={{ color: "white" }} class="fas fa-pen"></i>
+                                                                <i style={{ color: "white" }} class="fas fa-edit"></i>
                                                             </button>
                                                             <button className="btn btn-danger" onClick={() => {
                                                                 removeTask(todoList[selTodo]._id, index)
